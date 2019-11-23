8bcfe143cea4918ad27ff4bffb94d3cf3097498b
Maxence Dalmais <github@maxired.fr>
2019-11-23 16:10:29 +0100

    rename project deployement

--- src/apollo/clientFactory.js
+++ src/apollo/clientFactory.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
  const clientFactory  = (endpoint = 'graphql') => new ApolloClient({
-  uri: `https://gatsby-functions.maxired.now.sh/api/${endpoint}`,
+  uri: `https://lyonjs-optimistic-ui.maxired.now.sh/api/${endpoint}`,
   fetch,
 });
 
